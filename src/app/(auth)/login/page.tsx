f5659490c43489de519f8d99a94d5b23aa5bc62f
2bit-hash <gowasalmon@yahoo.com>
2024-08-05 16:22:00 +0300

    loginpage

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,14 @@
-import { Metadata } from "next";
-import LoginForm from "./LoginForm";
-import Link from "next/link";
 import loginImage from "@/assets/login-image.jpg";
+import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
+import GoogleSignInButton from "./google/GoogleSignInButton";
+import LoginForm from "./LoginForm";
 
 export const metadata: Metadata = {
   title: "Login",
 };
+
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
@@ -14,9 +16,15 @@ export default function Page() {
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
           <h1 className="text-center text-3xl font-bold">Login to safeSpace</h1>
           <div className="space-y-5">
+            <GoogleSignInButton />
+            <div className="flex items-center gap-3">
+              <div className="bg-muted h-px flex-1" />
+              <span>OR</span>
+              <div className="bg-muted h-px flex-1" />
+            </div>
             <LoginForm />
             <Link href="/signup" className="block text-center hover:underline">
-              Dont have an account Sign up?
+              Don&apos;t have an account? Sign up
             </Link>
           </div>
         </div>
